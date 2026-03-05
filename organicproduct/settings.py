8de9e7b955d3fb737857437d19982c02435adae8
Logakarthick0EB1F560A7FF79694F042F55ED1AE6BE <144759882+Logakarthick2004@users.noreply.github.com>
2026-03-05 17:10:44 +0530

    Change database from PostgreSQL to SQLite

--- organicproduct/settings.py
+++ organicproduct/settings.py
@@ -77,15 +77,10 @@ WSGI_APPLICATION = 'organicproduct.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dbname',
-        'USER': 'user',
-        'PASSWORD': 'password',
-        'HOST': 'host',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
