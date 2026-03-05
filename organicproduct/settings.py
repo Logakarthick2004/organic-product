0b41c3600179b93983b47d73757af9adccb09e92
Logakarthick0EB1F560A7FF79694F042F55ED1AE6BE <144759882+Logakarthick2004@users.noreply.github.com>
2026-03-05 17:21:51 +0530

    Set DEBUG to False in settings.py
    Changed DEBUG setting to False for production.

--- organicproduct/settings.py
+++ organicproduct/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4ux6l)1uh(_mt0&o&^dfni#jxznvuvu2l4m^qcao$ra@t6(k1v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "organic-product-hw61.onrender.com",
